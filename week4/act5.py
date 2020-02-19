266b6591840eaf22614fec8010d09f8f72857326
Ruangrith.Ri <ruangrith.ri@gmail.com>
2020-02-19 14:54:17 +0700

    week4 act 5 contrastimg

--- week4/act5.py
+++ week4/act5.py
@@ -1,5 +1,6 @@
 # importing cv2
 import cv2
+import numpy
 
 # path
 path = r'C:\Users\admin\Desktop\MDT425\week2\geeksforgeeks.png'
@@ -10,9 +11,9 @@ img = cv2.imread(path)
 
 width, height = img.shape[0:2]
 
-#Resize
-resizeImg = cv2.resize(img,(0,0),fx = 0.75,fy = 0.5)
+#Contrast
+contrastImg = cv2.addWeighted(img,2.5,numpy.zeros(img.shape,img.dtype),0,0)
 
 # Displaying the image
-cv2.imshow('Resize image', resizeImg)
+cv2.imshow('contrastImg', contrastImg)
 cv2.waitKey()
