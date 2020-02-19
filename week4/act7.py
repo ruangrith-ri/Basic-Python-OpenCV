319161b3a3e5bf27eeb1863c67ab0b6b995016fd
Ruangrith.Ri <ruangrith.ri@gmail.com>
2020-02-19 15:29:09 +0700

    wee4 act7 GuessianBlur

--- week4/act7.py
+++ week4/act7.py
@@ -1,1 +1,24 @@
+# importing cv2
+import cv2
+
+# path
+path = r'C:\Users\admin\Desktop\MDT425\week2\geeksforgeeks.png'
+
+# Using cv2.imread() method
+img = cv2.imread(path)
+
+#rows cols
+width, height = img.shape[0:2]
+
 #Guessian Blur
+blurImg = cv2.GaussianBlur(img,(3,3),0)
+cv2.imshow('3x3 filter',blurImg)
+
+blurImg = cv2.GaussianBlur(img,(5,5),0)
+cv2.imshow('5x5 filter',blurImg)
+
+blurImg = cv2.GaussianBlur(img,(7,7),0)
+cv2.imshow('7x7 filter',blurImg)
+
+cv2.imshow('Identity filter',img)
+cv2.waitKey()
--- week4/act8.py
+++ week4/act8.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 # path
-path = r'C:\Users\admin\Desktop\MDT425\week2\geeksforgeeks.png'
+path = r'C:\Users\admin\Desktop\MDT425\week2\export5-1.png'
 
 # Using cv2.imread() method
 img = cv2.imread(path)
@@ -11,7 +11,11 @@ img = cv2.imread(path)
 #rows cols
 width, height = img.shape[0:2]
 
+edgeImg = cv2.Canny(img,100,200)
+cv2.imshow('edge Image',edgeImg)
+edgeImg = cv2.Canny(img,200,200)
+cv2.imshow('edge Image2',edgeImg)
+
 # Displaying the image
 cv2.imshow('Original Image',img)
-cv2.imshow('Crpped Image',croppedImage)
 cv2.waitKey()
