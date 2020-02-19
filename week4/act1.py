028d38715d4dc3a2a54afedf9d323fc237884e2e
Ruangrith.Ri <ruangrith.ri@gmail.com>
2020-02-19 14:02:47 +0700

    act1 read type image

---
+++ week4/act1.py
@@ -0,0 +1,11 @@
+# importing cv2
+import cv2
+
+# path
+path = r'C:\Users\admin\Desktop\MDT425\week2\geeksforgeeks.png'
+
+# Using cv2.imread() method
+img = cv2.imread(path)
+
+#find data type
+print(type(img))
