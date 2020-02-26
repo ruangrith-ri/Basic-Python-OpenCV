0852eff2c218fbd8c25c3c7672b944dbdde30eee
Ruangrith.Ri <ruangrith.ri@gmail.com>
2020-02-26 17:08:13 +0700

    Week5 END

--- Week5 - CV2_ImageDetection/Act5 - CV2_BackgroundSubtraction.py
+++ Week5 - CV2_ImageDetection/Act5 - CV2_BackgroundSubtraction.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np 
 import cv2
 
+#FRAME DIFF
 path1 = r'C:\Users\admin\Desktop\MDT425\Week5 - CV2_ImageDetection\workshop\highway1.png'
 path2 = r'C:\Users\admin\Desktop\MDT425\Week5 - CV2_ImageDetection\workshop\highway2.png'
 
@@ -17,3 +18,40 @@ ret, mark = cv2.threshold(frame_diff,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 plt.imshow(mark,'gray')
 plt.show()
+
+
+#BACKGROUND SUBTRACTION
+path = r'C:\Users\admin\Desktop\MDT425\Week5 - CV2_ImageDetection\workshop\highway.mp4'
+pathExport = r'C:\Users\admin\Desktop\MDT425\Week5 - CV2_ImageDetection\workshop\highwayResult.mp4'
+
+capture = cv2.VideoCapture(path)
+output_file = (pathExport)
+
+frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter(output_file, fourcc, 25, (frame_width, frame_height))
+
+#CREATE MODEL
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+
+i = 0
+
+while(True):
+    has_frame, frame = capture.read()
+
+    i = i + 1
+
+    if not has_frame:
+        print('Reached the end of the video.')
+        print('Processed %d frames.'%i)
+        break
+    
+    #ESTIMATE BACKGROUND
+    fgmask = fgbg.apply(frame)
+    out = cv2.cvtColor(fgmask,cv2.COLOR_GRAY2BGR)
+    video.write(out)
+
+capture.release()
+video.release()
