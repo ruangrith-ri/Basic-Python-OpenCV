07fe5dbe1726ae11bf8c31b93156e7a6e6374af0
Ruangrith Rithruangdech <ruangrith.ri@gmail.com>
2020-03-18 15:14:41 +0700

    Act2 -EROSION/DILATION Update

--- Week6 - Morphology/Act1 - CV2_MorphologyText.py
+++ Week6 - Morphology/Act1 - CV2_MorphologyText.py
@@ -8,7 +8,6 @@ path1 = r'C:\Users\Jeeno\source\repos\python-CV-basic\Week6 - Morphology\img\inp
 img = cv2.imread(path1,0)
 kernel = np.ones((5,5),np.uint8)
 
-
 img_erosion = cv2.erode(img,kernel,iterations=1)
 img_dilation = cv2.dilate(img,kernel,iterations=1)
 
--- Week6 - Morphology/Act2 - CV2_MorphologyCoin.py
+++ Week6 - Morphology/Act2 - CV2_MorphologyCoin.py
@@ -1,13 +1,24 @@
 from matplotlib import pyplot as plt
 import cv2
-import os
 import numpy as np
 
 #PART IMAGE
 path1 = r'C:\Users\Jeeno\source\repos\python-CV-basic\Week6 - Morphology\img\coin.jpg'
 
-gray_image = cv2.imread(path1,0)
-thresh_val,thresh = cv2.threshold(gray_image,0,255,cv2.THRESH_OTSU)
-plt.axis('off')
-plt.imshow(thresh,'gray')
-plt.show()
+#Threshold
+img = cv2.imread(path1,0)
+thresh_val,img_thresh = cv2.threshold(img,0,255,cv2.THRESH_OTSU)
+
+#EROSION
+se = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+img_erosion =cv2.erode(img_thresh,se,iterations=1)
+
+#DILATION
+se = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+img_dilation =cv2.dilate(img_thresh,se,iterations=1)
+
+cv2.imshow('input',img_thresh)
+cv2.imshow('erosion',img_erosion)
+cv2.imshow('dilation',img_dilation)
+
+cv2.waitKey()
