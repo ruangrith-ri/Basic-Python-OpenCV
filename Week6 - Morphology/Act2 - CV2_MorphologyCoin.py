b1551589ac164526cefde60b86df4437db7740d4
Ruangrith Rithruangdech <ruangrith.ri@gmail.com>
2020-03-18 15:37:48 +0700

    Refactoring Act2

--- Week6 - Morphology/Act2 - CV2_MorphologyCoin.py
+++ Week6 - Morphology/Act2 - CV2_MorphologyCoin.py
@@ -1,4 +1,3 @@
-from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 
@@ -9,20 +8,19 @@ path1 = r'C:\Users\Jeeno\source\repos\python-CV-basic\Week6 - Morphology\img\coi
 img = cv2.imread(path1,0)
 thresh_val,img_thresh = cv2.threshold(img,0,255,cv2.THRESH_OTSU)
 
-#EROSION
+#SE
 se = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+
+#EROSION
 img_erosion =cv2.erode(img_thresh,se,iterations=1)
 
 #DILATION
-se = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 img_dilation =cv2.dilate(img_thresh,se,iterations=1)
 
 #OPENING - REMOVE NOISE
-se = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 img_open =cv2.morphologyEx(img_thresh,cv2.MORPH_OPEN,se)
 
 #CLOSEING - FILL HOLDS
-se = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 img_close =cv2.morphologyEx(img_thresh,cv2.MORPH_CLOSE,se)
 
 cv2.imshow('input',img_thresh)
