5022c35a069eb1a41ce1082676781cde71ad4206
Ruangrith Rithruangdech <ruangrith.ri@gmail.com>
2020-04-08 15:10:10 +0700

    Week 8 Act1

--- Week8 - Tracking/Act1.py
+++ Week8 - Tracking/Act1.py
@@ -17,7 +17,31 @@ frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 video = cv2.VideoWriter(output_file,fourcc,25,(frame_width,frame_height))
 
-cat_detect = cv2.CascadeClassifier(path_feature)
+has_frame, frame = capture.read()
+
+#SETUP INITIAL LOCATION WINDOWS
+r = 270
+h = 120
+c = 540
+w = 120
+
+track_window = (c,r,w,h)
+
+#SETUP ROI FOR TRACKING
+roi = frame[r:r+h,c:c+w]
+
+hsv_roi = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
+
+mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)),np.array((180., 255., 255.)))
+
+roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0,180])
+
+cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+
+#SETUP THE TERMINATION CRITERIA, EITHER 10 ITERATION OR MOVE BY ATLEAST 1 PT
+
+term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 2)
+
 
 i = 0
 while True:
@@ -28,14 +52,19 @@ while True:
         print('Processed %d frames.'% i)
         break
         
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    rects = cat_detect.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    dst = cv2.calcBackProject([hsv], [0], roi_hist, [0,180],  1)
+
+    #APPLY MEANSHIFT TO GET THE NEW LOCATION
+    ret, track_window = cv2.meanShift(dst, track_window, term_crit)
    
     ## print('Number of cat faces: %d'% len(rects))
     output = frame.copy()
-    for (i, (x, y, w, h)) in enumerate(rects):
-        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 10)
-        video.write(output)
+
+    x,y,w,h = track_window
+
+    cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 10)
+    video.write(output)
 
 capture.release()
 video.release()
